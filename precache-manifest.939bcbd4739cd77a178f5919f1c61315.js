9b0a058e828c5ef1c070badf0d58d26247326eab
Rahul Garg <rgrg27@gmail.com>
2020-08-03 18:47:05 +0530

    Updates

---
+++ precache-manifest.939bcbd4739cd77a178f5919f1c61315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5161a70289df00e008677f1cf97cbd1",
+    "url": "/Personal-Advisor-React-App/index.html"
+  },
+  {
+    "revision": "c9be1d4a41385d4d6d4a",
+    "url": "/Personal-Advisor-React-App/static/css/main.14f7c604.chunk.css"
+  },
+  {
+    "revision": "a91e9b5e09e20b095150",
+    "url": "/Personal-Advisor-React-App/static/js/2.d851aef4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Advisor-React-App/static/js/2.d851aef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9be1d4a41385d4d6d4a",
+    "url": "/Personal-Advisor-React-App/static/js/main.889a1564.chunk.js"
+  },
+  {
+    "revision": "a1eaf79bec3d0df89966",
+    "url": "/Personal-Advisor-React-App/static/js/runtime-main.c0b27819.js"
+  }
+]);
